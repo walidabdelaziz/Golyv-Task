4c17406d800fa32b3cc3cd436c99c9bcb85cc221
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2023-09-11 13:58:31 +0300

    add network manager

---
+++ Golyv Task/Consts.swift
@@ -0,0 +1,12 @@
+//
+//  Consts.swift
+//  Golyv Task
+//
+//  Created by Walid Ahmed on 11/09/2023.
+//
+
+import Foundation
+class Consts{
+    // MARK:- Repos
+    static var REPOS = "https://api.github.com/repositories"
+}
---
+++ Golyv Task/NetworkManager.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkManager.swift
+//  Golyv Task
+//
+//  Created by Walid Ahmed on 11/09/2023.
+//
+
+import Foundation
+import UIKit
+
+class NetworkManager {
+
+    static let shared = NetworkManager()
+    private init() {}
+    
+    enum NetworkError: Error {
+        case invalidURL
+        case requestFailed
+        case decodingError
+    }
+    
+    func request<T: Decodable>(from urlString: String) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw NetworkError.requestFailed
+        }
+    }
+
+
+    func loadImage(from urlString: String) async throws -> UIImage {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw NetworkError.decodingError
+            }
+        } catch {
+            throw NetworkError.requestFailed
+        }
+    }
+}
